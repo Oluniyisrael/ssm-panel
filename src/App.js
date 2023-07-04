8ed07c5d33fb298feb32584fafec2d7fde09582c
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-07-04 23:28:47 +0100

    Doing things

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import LandingPage from './pages/landingPage/LandingPage';
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import UserPage from './pages/userPage/UserPage';
+import { useState } from 'react';
 
 function App() {
-  const isLoggedIn = true
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
+  function setLoggedIn() {
+    setIsLoggedIn(true)
+  }
   return (
     <div>
    <Router>
     <Routes>
-      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/' element={<LandingPage
+      setLoggedIn = {setLoggedIn}/>}/>
       {isLoggedIn && <Route path='/user' element={<UserPage/>}/>}
     </Routes>
    </Router>
-{/* errand sending App */}
+{/* Smm app */}
     </div>
   );
 }
